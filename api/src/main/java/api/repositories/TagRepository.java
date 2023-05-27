22c803ed9678ad00e1825a27930f42d4cbdfe06f
jvitor07 <jvitoroliveira0711@gmail.com>
2023-05-27 20:35:50 -0300

    feature(backend): Cria o Repository para Tag

---
+++ api/src/main/java/api/repositories/TagRepository.java
@@ -0,0 +1,9 @@
+package api.repositories;
+
+import api.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
