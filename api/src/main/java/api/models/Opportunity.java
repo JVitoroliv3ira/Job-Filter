8af5491d2c30007239038ef1180b522db558147d
jvitor07 <jvitoroliveira0711@gmail.com>
2023-05-27 20:25:18 -0300

    feature(backend): Cria entidade Opportunity

---
+++ api/src/main/java/api/models/Opportunity.java
@@ -0,0 +1,46 @@
+package api.models;
+
+import api.enums.OpportunityType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(schema = "JOB_FILTER", name = "TB_OPPORTUNITIES")
+@Entity
+public class Opportunity {
+    @Id
+    @SequenceGenerator(name = "SQ_OPPORTUNITIES", schema = "JOB_FILTER", sequenceName = "SQ_OPPORTUNITIES", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_OPPORTUNITIES")
+    @Column(name = "ID_OPPORTUNITY")
+    private Long id;
+
+    @Column(name = "NM_OPPORTUNITY")
+    private String name;
+
+    @Column(name = "DS_OPPORTUNITY")
+    private String description;
+
+    @Column(name = "VL_SALARY")
+    private Long salary;
+
+    @Column(name = "TP_OPPORTUNITY")
+    private OpportunityType type;
+
+    @ManyToMany
+    @JoinTable(
+            schema = "JOB_FILTER",
+            name = "TB_OPPORTUNITY_TAG",
+            joinColumns = @JoinColumn(name = "ID_OPPORTUNITY"),
+            inverseJoinColumns = @JoinColumn(name = "ID_TAG")
+    )
+    private List<Tag> tags = new ArrayList<>();
+}
