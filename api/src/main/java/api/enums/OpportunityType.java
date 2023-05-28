e3a35d9a8c5eb4f41ea0f4b445a59d7d07df0ab2
jvitor07 <jvitoroliveira0711@gmail.com>
2023-05-27 22:48:17 -0300

    fix(backend): Ajusta enum de tipo de oportunidade

--- api/src/main/java/api/enums/OpportunityType.java
+++ api/src/main/java/api/enums/OpportunityType.java
@@ -1,14 +1,27 @@
 package api.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum OpportunityType {
-    REMOTE("Presencial"),
-    IN_PERSON("Remoto"),
+    REMOTE("Remoto"),
+    IN_PERSON("Presencial"),
     HYBRID("Híbrido");
 
+    private final String key;
     private final String value;
+
+    OpportunityType(String value) {
+        this.key = this.name();
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static OpportunityType deserialize(@JsonProperty("key") String key) {
+        return OpportunityType.valueOf(key);
+    }
 }
