1380ce8e654ea1935b6fe15848d8215b134f2cc3
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-01 21:37:33 -0300

    feature(frontend): Cria service abstrato para os services do frontend.

---
+++ web/src/app/api/services/abstract-api.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export abstract class AbstractApiService {
+    private readonly baseUrl = 'http://localhost:8080/api/v1';
+
+    constructor(private endpoint: string, private http: HttpClient) { }
+
+    public post<R, T>(path: string, data: T): Observable<R> {
+        return this.http.post<R>(`${this.baseUrl}/${this.endpoint}/${path}`, data);
+    }
+}
