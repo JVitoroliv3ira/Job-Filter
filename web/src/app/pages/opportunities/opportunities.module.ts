3137bda9a50377471917812abab1351506fd8bc8
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-02 21:15:11 -0300

    feature(frontend): Cria o modulo de rotas das oportunidades.

--- web/src/app/pages/opportunities/opportunities.module.ts
+++ web/src/app/pages/opportunities/opportunities.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { OpportunitiesListComponent } from './opportunities-list/opportunities-list.component';
 import { CommonModule } from '@angular/common';
+import { OpportunitiesRoutingModule } from './opportunities-routing.module';
 
 @NgModule({
-    declarations: [
+  declarations: [
     OpportunitiesListComponent
   ],
   exports: [
     OpportunitiesListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    OpportunitiesRoutingModule
   ]
 })
 export class OpportunitiesModule { }
