8eabb7c3f3040e6e47ad0a65be0fe77183094094
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-03 22:02:57 -0300

    feature(frontend): Utiliza filtro de oportunidades na pagina de listagem das oportunidades.

--- web/src/app/pages/opportunities/opportunities-list/opportunities-list.component.ts
+++ web/src/app/pages/opportunities/opportunities-list/opportunities-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OpportunityFilterDTO } from 'src/app/api/dtos/opportunity-filter.dto';
 
 @Component({
   selector: 'app-opportunities-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./opportunities-list.component.scss']
 })
 export class OpportunitiesListComponent {
-
+  public handleFilter(filter: OpportunityFilterDTO): void {
+    console.log(filter)
+  }
 }
