b00d4d0219c85a7866db0d147892c06cd169563f
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-03 22:23:25 -0300

    feature(frontend): Realiza a implementacao inicial do filtro de oportunidades.

--- web/src/app/pages/opportunities/opportunities-list/opportunities-list.component.ts
+++ web/src/app/pages/opportunities/opportunities-list/opportunities-list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
 import { OpportunityFilterDTO } from 'src/app/api/dtos/opportunity-filter.dto';
+import { OpportunitiesService } from 'src/app/api/services/opportunities.service';
 
 @Component({
   selector: 'app-opportunities-list',
   templateUrl: './opportunities-list.component.html',
   styleUrls: ['./opportunities-list.component.scss']
 })
-export class OpportunitiesListComponent {
-  public handleFilter(filter: OpportunityFilterDTO): void {
-    console.log(filter)
+export class OpportunitiesListComponent implements OnInit {
+  public filter = { pageNumber: 0, pageSize: 15 } as OpportunityFilterDTO;
+
+  constructor(private opportunitiesService: OpportunitiesService) {}
+
+  ngOnInit(): void {
+    this.searchOpportunities();
+  }
+
+  public handleFilterEvent(filter: OpportunityFilterDTO): void {
+    this.filter.name = filter.name;
+    this.filter.description = filter.description;
+    this.filter.companyName = filter.companyName;
+    this.filter.salary = filter.salary;
+    this.filter.type = filter.type;
+    this.filter.tag = filter.tag;
+
+    this.searchOpportunities();
+  }
+
+  private searchOpportunities(): void {
+    this.opportunitiesService
+      .searchOpportunities(this.filter)
+      .pipe(take(1))
+      .subscribe(res => console.log(res));
   }
 }
