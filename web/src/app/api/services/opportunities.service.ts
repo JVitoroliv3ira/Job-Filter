3198731c1a9afb88086571e5ca197a0a150542b7
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-03 22:21:23 -0300

    feature(frontend): Adiciona metodo para buscar oportunidades no opportunities.service.

--- web/src/app/api/services/opportunities.service.ts
+++ web/src/app/api/services/opportunities.service.ts
@@ -4,6 +4,9 @@ import { HttpClient } from "@angular/common/http";
 import { AbstractApiService } from "./abstract-api.service";
 import { OpportunityType } from "../enums/opportunity-type";
 import { Observable } from "rxjs";
+import { OpportunityFilterDTO } from "../dtos/opportunity-filter.dto";
+import { PaginatedResultDTO } from "../dtos/paginated-result.dto";
+import { Opportunity } from "../models/opportunity";
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +14,10 @@ import { Observable } from "rxjs";
 export class OpportunitiesService extends AbstractApiService {
     constructor(http: HttpClient) {
         super('opportunities', http);
+    }
+
+    public searchOpportunities(filter: OpportunityFilterDTO): Observable<PaginatedResultDTO<Opportunity>> {
+        return this.post<PaginatedResultDTO<Opportunity>, OpportunityFilterDTO>('search', filter);
     }
 
     public getAllOpportunitiesTypes(): Observable<Array<OpportunityType>> {
