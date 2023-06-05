9340d0c0cb752c547d54cd24bb12ef2388afa6af
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-05 20:46:53 -0300

    feature(frontend): Utiliza o evento de filtro de empresas.

--- web/src/app/pages/companies/companies-list/companies-list.component.ts
+++ web/src/app/pages/companies/companies-list/companies-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CompanyFilterDTO } from 'src/app/api/dtos/company-filter.dto';
+import { PaginatedResultDTO } from 'src/app/api/dtos/paginated-result.dto';
+import { Company } from 'src/app/api/models/company';
 
 @Component({
   selector: 'app-companies-list',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./companies-list.component.scss']
 })
 export class CompaniesListComponent {
+  public filter = { pageNumber: 0, pageSize: 5 } as CompanyFilterDTO;
+  public loadingSearch = true;
+  public paginatedResult: PaginatedResultDTO<Company> = {} as PaginatedResultDTO<Company>;
 
+  public handleFilterEvent(filter: CompanyFilterDTO): void {
+    this.filter.name = filter.name;
+    this.filter.description = filter.description;
+    this.filter.email = filter.email;
+    this.filter.city = filter.city;
+  }
 }
