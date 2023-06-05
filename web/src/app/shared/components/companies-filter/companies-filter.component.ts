b33b83b03839f9cc9495cacba400d6414adcc592
jvitor07 <jvitoroliveira0711@gmail.com>
2023-06-05 20:32:09 -0300

    feature(frontend): Adiciona os campos do DTO no filtro de empresas.

--- web/src/app/shared/components/companies-filter/companies-filter.component.ts
+++ web/src/app/shared/components/companies-filter/companies-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CompanyFilterDTO } from 'src/app/api/dtos/company-filter.dto';
 
 @Component({
   selector: 'app-companies-filter',
@@ -7,8 +8,14 @@ import { Component } from '@angular/core';
 })
 export class CompaniesFilterComponent {
   public advancedFilterIsExpanded = false;
+  public filter = {} as CompanyFilterDTO;
+  @Output() filterEvent = new EventEmitter<CompanyFilterDTO>();
 
   public toggleAdvancedFilterVisibility(): void {
     this.advancedFilterIsExpanded = !this.advancedFilterIsExpanded;
+  }
+
+  public submit(): void {
+    this.filterEvent.emit(this.filter);
   }
 }
